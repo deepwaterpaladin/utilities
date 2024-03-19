df7ee8eabc24ececf50d2387ecf12a83b8daea9e
Rielly Young <67346803+deepwaterpaladin@users.noreply.github.com>
2024-03-19 12:16:28 -0400

    Create cloud_transfer.py

---
+++ cloud_transfer.py
@@ -0,0 +1,8 @@
+class CloudTransfer:
+    '''
+    Transfers files to a cloud storage provider (Sharepoint, Google Drive, etc.).
+    Maintains file format.
+    '''
+
+    def __init__(self) -> None:
+        pass
